722ee4b1156027c3a424038021b5ed274a7c0131
vboxuser <vboxuser@Debian2.myguest.virtualbox.org>
2025-12-14 15:13:44 -0600

    Complete basic parser

--- src/parse.c
+++ src/parse.c
@@ -7,6 +7,10 @@ int parse_line(char *line, Job *job) {
     int argc = 0;
     int capacity = 8;
 
+    job->background = 0;
+    job->has_pipe = 0;
+    job->rawline = NULL;
+
     job->left.argv = malloc(capacity * sizeof(char *));
     if (!job->left.argv)
         return -1;
@@ -14,9 +18,16 @@ int parse_line(char *line, Job *job) {
     char *token = strtok(line, " \t");
 
     while (token) {
+        if (strcmp(token, "&") == 0) {
+            job->background = 1;
+            token = strtok(NULL, " \t");
+            continue;
+        }
+
         if (argc >= capacity - 1) {
             capacity *= 2;
-            char **tmp = realloc(job->left.argv, capacity * sizeof(char *));
+            char **tmp = realloc(job->left.argv,
+                                  capacity * sizeof(char *));
             if (!tmp)
                 return -1;
             job->left.argv = tmp;
@@ -31,17 +42,17 @@ int parse_line(char *line, Job *job) {
     }
 
     job->left.argv[argc] = NULL;
-    job->has_pipe = 0;
-    job->background = 0;
-    job->rawline = NULL;
-
     return 0;
 }
 
 void free_job(Job *job) {
+    if (!job)
+        return;
+
     if (job->left.argv) {
-        for (int i = 0; job->left.argv[i]; i++)
+        for (int i = 0; job->left.argv[i]; i++) {
             free(job->left.argv[i]);
+        }
         free(job->left.argv);
     }
 }
